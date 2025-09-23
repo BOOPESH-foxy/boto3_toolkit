5a22aa8efc6deb87cb08f4d184d598adaaaa60f0
boopesh-foxy <boopesh.mc@gmail.com>
2025-09-23 15:36:39 +0530

    instances create funtion

--- ec2/ec2_instance.py
+++ ec2/ec2_instance.py
@@ -1,6 +1,12 @@
-from ec2_resource import ec2_client
+import os
 import vpc
+from ec2_resource import ec2_client
+from dotenv import load_dotenv
 
+load_dotenv()
+INSTANCE_TYPE=os.getenv('INSTANCE_TYPE')
+KEY_NAME = os.getenv('KEY_NAME')
+INSTANCE_NAME=os.getenv('INSTANCE_NAME')
 ec2 = ec2_client()
 
 
@@ -11,8 +17,8 @@ OS_OPTIONS = {
 
 def get_ami_id(os_choise_num: str):
     os_choise = OS_OPTIONS[os_choise_num]
+    print(f"! Fetching AMI id for {os_choise['name']}")
     os_id_filter = os_choise["filter_name"]
-    print(os_id_filter)
     response_instances = ec2.describe_images(
         Owners=['amazon'],
         Filters=[
@@ -22,7 +28,54 @@ def get_ami_id(os_choise_num: str):
     )
     images = response_instances['Images'][0]
     image_id = images['ImageId']
-    print(image_id)
+    return image_id
 
-def create_ec2_instaces():
-    pass
+
+def check_instance_existence():
+    print(f"! Checking for {INSTANCE_NAME} existence")
+    response_instance_existence = ec2.describe_instances(
+        Filters=[{
+            'Name':'tag:Name','Values':[INSTANCE_NAME],
+            'Name':'instance-state-name','Values':['running'],
+        }]
+    )
+    images = response_instance_existence['Reservations']
+    if(images):
+        images_data = images[0]['Instances']
+        instanceId = images_data[0]['InstanceId']
+        print("! Instance already exists with given name !")
+        return instanceId
+    else:
+        print(f"! Instance {INSTANCE_NAME} doesnt exists, creating one .. ")
+        # create_ec2_instance()
+    
+
+
+def create_ec2_instance():
+    print("! Creating an EC2 instance")
+    print("\n! Setting Up VPC resource ")
+    vpc_setup_results = vpc.setup_vpc_resources()
+    subnet_id = vpc_setup_results['subnet_id']
+    sg_id = vpc_setup_results['sg_id']
+
+    response_run_instances = ec2.run_instances(
+        ImageId=get_ami_id('1'),
+        InstanceType=INSTANCE_TYPE,
+        MaxCount=1,
+        MinCount=1,
+        KeyName=KEY_NAME,
+        SubnetId=subnet_id,
+        SecurityGroupIds=[sg_id],
+        TagSpecifications=[{
+            'ResourceType': 'instance',
+            'Tags': [{'Key': 'Name', 'Value': INSTANCE_NAME}]
+        }]
+    )
+    instance_id=response_run_instances['Instances'][0]
+    id=instance_id['InstanceId']
+    print(f"+ Created instance {INSTANCE_NAME} id={id}")
+    return id
+
+
+check_instance_existence()
+# create_ec2_instance()
--- ec2/vpc.py
+++ ec2/vpc.py
@@ -224,10 +224,22 @@ def create_subnets_for_vpc(vpc_id: str,az_name: str,az_id: str):
             print("::Error::",e)
             raise
 
-# if __name__ == "__main__":
-#     os.system('clear')
-#     vpc_id = create_vpc()
-#     sg_id = create_security_group(vpc_id)
-#     igw_id = create_internet_gateway(vpc_id)
-#     az_name,az_id=get_availability_zones()
-#     subnet_id = create_subnets_for_vpc(vpc_id,az_name,az_id)
+def setup_vpc_resources():
+    """
+    Create/check VPC, SG, IGW, Subnet and return their IDs
+    """
+    vpc_id = create_vpc()
+    sg_id = create_security_group(vpc_id)
+    igw_id = create_internet_gateway(vpc_id)
+    az_name, az_id = get_availability_zones()
+    subnet_id = create_subnets_for_vpc(vpc_id, az_name, az_id)
+    
+    return {
+        "vpc_id": vpc_id,
+        "sg_id": sg_id,
+        "igw_id": igw_id,
+        "subnet_id": subnet_id,
+        "az_name": az_name,
+        "az_id": az_id
+    }
+
